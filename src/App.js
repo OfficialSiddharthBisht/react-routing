f7cd70edf0c53b268ce29375629ab526eee0229c
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-03 01:13:12 +0530

    added pages and done routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import ProductPage from './components/ProductPage'
+import Products from './components/Product'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Routes>
+        <Route path='/' element = {<Home/>}></Route>
+        <Route path = '/Product' element = {<Products/>}></Route>
+        <Route path = './Product/:id' element = {<ProductPage/>}></Route>
+      </Routes>
     </div>
   );
 }
