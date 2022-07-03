a1ae85d71d80a1943816bdc7d8e38a2b211becc4
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-03 10:42:44 +0530

    made the navbar with routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import {Box} from '@chakra-ui/react';
+import {Link} from 'react-router-dom';
 
 function Navbar(){
     return(
-        <>
-            Navbar
-        </>
+        <Box
+            display= "flex"
+            flexDirection= "row"
+            padding="12px"
+            border= "1px solid black"
+            borderRadius = "10px"
+            justifyContent = "space-between"
+        >
+            <Link style={{textDecoration:"none"}} to ="/">Home</Link>
+            <Link style={{textDecoration: "none"}} to = "/product">Products</Link>
+        </Box>
     )
 }
 
